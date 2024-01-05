770c56747dea863a8b9ff756af1851532da04387
Ben Gruey <91962409+bgruey@users.noreply.github.com>
2024-01-05 12:25:59 -0600

    empty project, moved to phyllis

--- valgrind/dancer/signal_processors.h
+++ valgrind/dancer/signal_processors.h
@@ -1,6 +1,8 @@
 #ifndef _SIGNAL_PROCESSORS_H
 #define _SIGNAL_PROCESSORS_H
+
 #include <stdio.h>
+
 double low_pass_filter(double x_i, double y_im1, double alpha);
 
 double shunted_integrator(double y_int, double y, double dt, double lambda);
